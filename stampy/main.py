afeb29e94211c448a92ac27d64fec76bef34b09b
Comamoca <comamoca.dev@gmail.com>
2022-08-29 22:03:16 +0900

    :sparkles: (stampy/main.py) コマンドのデフォルト実行時の動作を変更しました。

--- stampy/main.py
+++ stampy/main.py
@@ -5,7 +5,7 @@ import random
 from pathlib import Path
 
 
-
+def set_random_color() -> tuple[int, ...]:
     def rand_col(num: int):
         num = random.randint(0, 255)
         return num
@@ -21,6 +21,7 @@ from pathlib import Path
 
 def divLine(txt: str) -> tuple[str, str]:
     if len(txt) != 4:
+        raise ValueError("テキストの長さが不正です。")
 
     line = []
     line2 = []
@@ -54,26 +55,44 @@ def to_tuple(txt: str) -> tuple[int, ...]:
         raise ValueError("渡された文字列の書式が不正です。")
         os.exit(1)
 
+    return tuple(int_list)
 
+
+def toRGB(hex_color: str) -> tuple[int, int, int]:
+    if hex_color[0] != "#":
+        # 16進数では無い
+        raise ValueError("引数が16進数ではありません。")
+
+    R = int(hex_color[1:3], 16)
+    G = int(hex_color[3:5], 16)
+    B = int(hex_color[5:7], 16)
+
+    return (R, G, B)
 
 
 @click.command()
 @click.argument("text")
-@click.option('--color', help='set color', default="0, 0, 0")
+@click.option("--color", help="set color", default="")
+@click.option("--ishex", help="set color to hex", is_flag=True)
 # @click.argument("text")
-def main(text: str, color: str = "0, 0, 0"):
+def main(text: str, color: str = "", ishex: bool = False):
     font_path = str(Path().cwd() / Path("./fonts/rounded-x-mplus-1c-bold.ttf"))
     font_size = 100
 
-    tup_color = to_tuple(color)
-    text, text2 = divLine(text)
-
-    if color == "0, 0, 0":
+    if len(color) >= 0:
         tup_color = set_random_color()
+
+    if ishex:
+        tup_color = toRGB(color)
+
+    elif len(color) != 0:
+        tup_color = to_tuple(color)
 
     # color = (255, 255, 255)
     # color = (0, 0, 0)
     # blue  = (131, 217, 242)
+
+    text, text2 = divLine(text)
 
     image = Image.new("RGB", (200, 200), (255, 255, 255))
 
